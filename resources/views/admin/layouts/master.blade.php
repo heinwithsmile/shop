8faa86997fe39b339b3f411a188281680a17bc7f
heinwithsmile <heinhtetaung7715@gmail.com>
2024-01-09 15:48:44 +0630

    modified some view template

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -1,1 +1,32 @@
-Hello!
+<!DOCTYPE html>
+<html lang="en">
+
+@include('admin.layouts.head')
+
+<body id="page-top">
+
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="">
+
+      <!-- Main Content -->
+      <div id="content">
+
+        <!-- Topbar -->
+        @include('admin.layouts.header')
+        <!-- End of Topbar -->
+
+        <!-- Begin Page Content -->
+        @yield('main-content')
+        <!-- /.container-fluid -->
+
+      </div>
+    </div>
+      <!-- End of Main Content -->
+      {{-- @include('admin.layouts.footer') --}}
+    
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 /*
@@ -25,3 +26,6 @@ Route::get('/', [HomeController::class, 'index']);
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Admin Routes
+Route::get('/admin', [AdminController::class, 'index']);
